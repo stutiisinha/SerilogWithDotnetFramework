ca15a78331798e9b0c90cc6ed4f075ef590aba30
stutiisinha <sinhastuti200128@gmail.com>
2023-09-07 17:00:08 +0530

    attempted serilog in .net framework 4.8

---
+++ SerilogWithDotnetFrameworkApp/Controllers/CalculatorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web.Http;
+
+namespace CalculatorApi.Controllers
+{
+    public class CalculatorController : ApiController
+    {
+        [HttpGet]
+        [Route("api/calculator/add")]
+        public IHttpActionResult Add(double num1, double num2)
+        {
+            double result = num1 + num2;
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("api/calculator/subtract")]
+        public IHttpActionResult Subtract(double num1, double num2)
+        {
+            double result = num1 - num2;
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("api/calculator/multiply")]
+        public IHttpActionResult Multiply(double num1, double num2)
+        {
+            double result = num1 * num2;
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("api/calculator/divide")]
+        public IHttpActionResult Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                return BadRequest("Division by zero is not allowed.");
+            }
+
+            double result = num1 / num2;
+            return Ok(result);
+        }
+    }
+}
---
+++ SerilogWithDotnetFrameworkApp/Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using System.Web;
+using System.Web.Http;
+using Serilog;
+using Serilog.Sinks.Console;
+
+
+namespace SerilogWithDotnetFrameworkApp
+{
+    public class Global : HttpApplication
+    {
+        private static bool headerLogged = false;
+
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(new Serilog.Sinks.Console.ConsoleSink.ConsoleSinkConfiguration
+                {
+                    OutputTemplate = !headerLogged ? GetHeaderTemplate() : "{Timestamp:yyyy-MM-dd HH:mm:ss.ffff} | {Level:u3} | {SourceContext} | {RequestId} | {Message}{NewLine}"
+                })
+                .CreateLogger();
+        }
+
+        private static string GetHeaderTemplate()
+        {
+            headerLogged = true;
+            return "======================================================================================================================================================\n" +
+                "Logging start    = {Timestamp:yyyy-MM-dd HH:mm:ss.ffff}\n" +
+                "Machine name     = {MachineName}\n" +
+                "Version          = 0.0.0.0\n" +
+                "AssemblyVersion  = {AssemblyVersion}\n" +
+                "Windows Version  = {WindowsVersion}\n" +
+                "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n" +
+                "Date and Time            | Level | Product              | Module                                                     | Message\n" +
+                "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------";
+        }
+    }
+}
